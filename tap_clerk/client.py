bfd40c4724f72145475ad3c4ae50291f73ecece5
Derek Visch <dvisch@autoidm.com>
2025-01-07 11:03:37 -0500

    Initial migration from KeraganHQ

--- tap_clerk/client.py
+++ tap_clerk/client.py
@@ -8,7 +8,7 @@ from importlib import resources
 
 from singer_sdk.authenticators import BearerTokenAuthenticator
 from singer_sdk.helpers.jsonpath import extract_jsonpath
-from singer_sdk.pagination import BaseAPIPaginator  # noqa: TC002
+from singer_sdk.pagination import BaseOffsetPaginator
 from singer_sdk.streams import RESTStream
 
 if t.TYPE_CHECKING:
@@ -28,12 +28,12 @@ class ClerkStream(RESTStream):
 
     # Update this value if necessary or override `get_new_paginator`.
     next_page_token_jsonpath = "$.next_page"  # noqa: S105
+    API_LIMIT_PAGE_SIZE = 100
 
     @property
     def url_base(self) -> str:
         """Return the API URL root, configurable via tap settings."""
-        # TODO: hardcode a value here, or retrieve it from self.config
-        return "https://api.mysample.com"
+        return "https://api.clerk.com/v1"
 
     @property
     def authenticator(self) -> BearerTokenAuthenticator:
@@ -47,72 +47,17 @@ class ClerkStream(RESTStream):
             token=self.config.get("auth_token", ""),
         )
 
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed.
+    def get_new_paginator(self) -> BaseOffsetPaginator:
+        return BaseOffsetPaginator(start_value=0, page_size=self.API_LIMIT_PAGE_SIZE)
 
-        Returns:
-            A dictionary of HTTP headers.
-        """
-        # If not using an authenticator, you may also provide inline auth headers:
-        # headers["Private-Token"] = self.config.get("auth_token")  # noqa: ERA001
-        return {}
-
-    def get_new_paginator(self) -> BaseAPIPaginator:
-        """Create a new pagination helper instance.
-
-        If the source API can make use of the `next_page_token_jsonpath`
-        attribute, or it contains a `X-Next-Page` header in the response
-        then you can remove this method.
-
-        If you need custom pagination that uses page numbers, "next" links, or
-        other approaches, please read the guide: https://sdk.meltano.com/en/v0.25.0/guides/pagination-classes.html.
-
-        Returns:
-            A pagination helper instance.
-        """
-        return super().get_new_paginator()
-
-    def get_url_params(
-        self,
-        context: Context | None,  # noqa: ARG002
-        next_page_token: t.Any | None,  # noqa: ANN401
-    ) -> dict[str, t.Any]:
-        """Return a dictionary of values to be used in URL parameterization.
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary of URL query parameters.
-        """
-        params: dict = {}
+    def get_url_params(self, context: dict | None, next_page_token: Any | None) -> dict[str, Any]:
+        params: dict = {"limit": self.API_LIMIT_PAGE_SIZE}
         if next_page_token:
-            params["page"] = next_page_token
+            params["offset"] = next_page_token
         if self.replication_key:
-            params["sort"] = "asc"
-            params["order_by"] = self.replication_key
+            params["order_by"] = f'-{self.replication_key}'
         return params
 
-    def prepare_request_payload(
-        self,
-        context: Context | None,  # noqa: ARG002
-        next_page_token: t.Any | None,  # noqa: ARG002, ANN401
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
 
     def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
         """Parse the response and return an iterator of result records.
@@ -123,25 +68,7 @@ class ClerkStream(RESTStream):
         Yields:
             Each record from the source.
         """
-        # TODO: Parse response body and return a set of records.
         yield from extract_jsonpath(
             self.records_jsonpath,
             input=response.json(parse_float=decimal.Decimal),
         )
-
-    def post_process(
-        self,
-        row: dict,
-        context: Context | None = None,  # noqa: ARG002
-    ) -> dict | None:
-        """As needed, append or transform raw data to match expected structure.
-
-        Args:
-            row: An individual record from the stream.
-            context: The stream context.
-
-        Returns:
-            The updated record dictionary, or ``None`` to skip the record.
-        """
-        # TODO: Delete this method if not needed.
-        return row
--- tap_clerk/streams.py
+++ tap_clerk/streams.py
@@ -1,66 +1,227 @@
-"""Stream type classes for tap-clerk."""
-
 from __future__ import annotations
 
+import sys
 import typing as t
-from importlib import resources
 
-from singer_sdk import typing as th  # JSON Schema typing helpers
+import requests
+from singer_sdk import typing as th
+from singer_sdk.helpers.jsonpath import extract_jsonpath
 
 from tap_clerk.client import ClerkStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = resources.files(__package__) / "schemas"
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
+if sys.version_info >= (3, 9):
+    import importlib.resources as importlib_resources
+else:
+    import importlib_resources
 
+
+class OrganizationsStream(ClerkStream):
+    """Organizations stream class."""
+    name = "organizations"
+    path = "/organizations"
+    primary_keys: t.ClassVar[list[str]] = ["id"]
+    replication_key = None
+    schema = th.PropertiesList(
+        th.Property("object", th.StringType),
+        th.Property("id", th.StringType),
+        th.Property("name", th.StringType),
+        th.Property("slug", th.StringType),
+        th.Property("members_count", th.IntegerType),
+        th.Property("max_allowed_memberships", th.IntegerType),
+        th.Property("admin_delete_enabled", th.BooleanType),
+        th.Property("public_metadata", th.ObjectType(additional_properties=True)),
+        th.Property("private_metadata", th.ObjectType(additional_properties=True)),
+        th.Property("created_by", th.StringType),
+        th.Property("created_at", th.IntegerType),
+        th.Property("updated_at", th.IntegerType),
+    ).to_dict()
+
+    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
+        yield from extract_jsonpath("$.data[*]", input=response.json())
+
+    def get_child_context(self, record: dict, context: t.Optional[dict]) -> dict:
+        return { "organization_id": record["id"] }
+
+class OrganizationMembershipStream(ClerkStream):
+    """OrganizationMembership stream class."""
+    name = "organization_membership"
+    parent_stream_type = OrganizationsStream
+    ignore_parent_replication_keys = True
+    path = "/organizations/{organization_id}/memberships"
+    primary_keys = ["id"]
+    replication_key = None
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType, description="The unique identifier for the membership"),
+        th.Property("object", th.StringType, description="Type of the object, should be 'organization_membership'"),
+        th.Property("role", th.StringType, description="Role within the organization"),
+        th.Property("permissions", th.ArrayType(th.StringType), description="List of permissions"),
+        th.Property("public_metadata", th.ObjectType(additional_properties=True), description="Public metadata"),
+        th.Property("private_metadata", th.ObjectType(additional_properties=True), description="Private metadata"),
+        th.Property("organization", th.ObjectType(
+            th.Property("object", th.StringType),
+            th.Property("id", th.StringType),
+            th.Property("name", th.StringType),
+            th.Property("slug", th.StringType),
+            th.Property("members_count", th.IntegerType),
+            th.Property("max_allowed_memberships", th.IntegerType),
+            th.Property("admin_delete_enabled", th.BooleanType),
+            th.Property("public_metadata", th.ObjectType(additional_properties=True)),
+            th.Property("private_metadata", th.ObjectType(additional_properties=True)),
+            th.Property("created_by", th.StringType),
+            th.Property("created_at", th.IntegerType),
+            th.Property("updated_at", th.IntegerType)
+        ), description="Embedded organization object"),
+        th.Property("public_user_data", th.ObjectType(
+            th.Property("user_id", th.StringType),
+            th.Property("first_name", th.StringType),
+            th.Property("last_name", th.StringType),
+            th.Property("profile_image_url", th.StringType),
+            th.Property("image_url", th.StringType),
+            th.Property("has_image", th.BooleanType),
+            th.Property("identifier", th.StringType)
+        ), description="Public data of the user associated with the membership"),
+        th.Property("created_at", th.IntegerType, description="Timestamp of when the membership was created"),
+        th.Property("updated_at", th.IntegerType, description="Timestamp of when the membership was last updated")
+    ).to_dict()
+
+    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
+        yield from extract_jsonpath("$.data[*]", input=response.json())
 
 class UsersStream(ClerkStream):
-    """Define custom stream."""
-
+    """Users stream class."""
     name = "users"
     path = "/users"
     primary_keys: t.ClassVar[list[str]] = ["id"]
     replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID",
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
+        th.Property("id", th.StringType, description="The unique identifier for a user"),
+        th.Property("object", th.StringType),
+        th.Property("external_id", th.StringType),
+        th.Property("primary_email_address_id", th.StringType),
+        th.Property("primary_phone_number_id", th.StringType),
+        th.Property("primary_web3_wallet_id", th.StringType),
+        th.Property("username", th.StringType),
+        th.Property("first_name", th.StringType),
+        th.Property("last_name", th.StringType),
+        th.Property("profile_image_url", th.StringType),
+        th.Property("image_url", th.StringType),
+        th.Property("has_image", th.BooleanType),
+        th.Property("public_metadata", th.ObjectType(additional_properties=True)),
+        th.Property("private_metadata", th.ObjectType(additional_properties=True)),
+        th.Property("unsafe_metadata", th.ObjectType(additional_properties=True)),
+        th.Property("email_addresses", th.ArrayType(th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("object", th.StringType),
+            th.Property("email_address", th.StringType),
+            th.Property("reserved", th.BooleanType),
+            th.Property("verification", th.ObjectType(
+                th.Property("status", th.StringType),
+                th.Property("strategy", th.StringType),
+                th.Property("attempts", th.IntegerType),
+                th.Property("expire_at", th.IntegerType)
+            )),
+            th.Property("linked_to", th.ArrayType(th.ObjectType(
+                th.Property("type", th.StringType),
+                th.Property("id", th.StringType)
+            ))),
+            th.Property("created_at", th.IntegerType),
+            th.Property("updated_at", th.IntegerType)
+        ))),
+        th.Property("phone_numbers", th.ArrayType(th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("object", th.StringType),
+            th.Property("phone_number", th.StringType),
+            th.Property("reserved_for_second_factor", th.BooleanType),
+            th.Property("default_second_factor", th.BooleanType),
+            th.Property("reserved", th.BooleanType),
+            th.Property("verification", th.ObjectType(
+                th.Property("status", th.StringType),
+                th.Property("strategy", th.StringType),
+                th.Property("attempts", th.IntegerType),
+                th.Property("expire_at", th.IntegerType)
+            )),
+            th.Property("linked_to", th.ArrayType(th.ObjectType(
+                th.Property("type", th.StringType),
+                th.Property("id", th.StringType)
+            ))),
+            th.Property("backup_codes", th.ArrayType(th.StringType)),
+            th.Property("created_at", th.IntegerType),
+            th.Property("updated_at", th.IntegerType)
+        ))),
+        th.Property("web3_wallets", th.ArrayType(th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("object", th.StringType),
+            th.Property("web3_wallet", th.StringType),
+            th.Property("verification", th.ObjectType(
+                th.Property("status", th.StringType),
+                th.Property("strategy", th.StringType),
+                th.Property("nonce", th.StringType),
+                th.Property("attempts", th.IntegerType),
+                th.Property("expire_at", th.IntegerType)
+            )),
+            th.Property("created_at", th.IntegerType),
+            th.Property("updated_at", th.IntegerType)
+        ))),
+        th.Property("passkeys", th.ArrayType(th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("object", th.StringType),
+            th.Property("name", th.StringType),
+            th.Property("last_used_at", th.IntegerType),
+            th.Property("verification", th.ObjectType(
+                th.Property("status", th.StringType),
+                th.Property("strategy", th.StringType),
+                th.Property("nonce", th.StringType),
+                th.Property("attempts", th.IntegerType),
+                th.Property("expire_at", th.IntegerType)
+            ))
+        ))),
+        th.Property("password_enabled", th.BooleanType),
+        th.Property("two_factor_enabled", th.BooleanType),
+        th.Property("totp_enabled", th.BooleanType),
+        th.Property("backup_code_enabled", th.BooleanType),
+        th.Property("external_accounts", th.ArrayType(th.ObjectType(additional_properties=True))),
+        th.Property("saml_accounts", th.ArrayType(th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("object", th.StringType),
+            th.Property("provider", th.StringType),
+            th.Property("active", th.BooleanType),
+            th.Property("email_address", th.StringType),
+            th.Property("first_name", th.StringType),
+            th.Property("last_name", th.StringType),
+            th.Property("provider_user_id", th.StringType),
+            th.Property("public_metadata", th.ObjectType(additional_properties=True)),
+            th.Property("verification", th.ObjectType(
+                th.Property("status", th.StringType),
+                th.Property("strategy", th.StringType),
+                th.Property("external_verification_redirect_url", th.StringType),
+                th.Property("error", th.ObjectType(
+                    th.Property("message", th.StringType),
+                    th.Property("long_message", th.StringType),
+                    th.Property("code", th.StringType),
+                    th.Property("meta", th.ObjectType(additional_properties=True)),
+                    th.Property("clerk_trace_id", th.StringType)
+                )),
+                th.Property("expire_at", th.IntegerType),
+                th.Property("attempts", th.IntegerType)
+            ))
+        ))),
+        th.Property("last_sign_in_at", th.IntegerType),
+        th.Property("banned", th.BooleanType),
+        th.Property("locked", th.BooleanType),
+        th.Property("lockout_expires_in_seconds", th.IntegerType),
+        th.Property("verification_attempts_remaining", th.IntegerType),
+        th.Property("updated_at", th.IntegerType),
+        th.Property("created_at", th.IntegerType),
+        th.Property("delete_self_enabled", th.BooleanType),
+        th.Property("create_organization_enabled", th.BooleanType),
+        th.Property("last_active_at", th.IntegerType)
     ).to_dict()
 
-
-class GroupsStream(ClerkStream):
-    """Define custom stream."""
-
-    name = "groups"
-    path = "/groups"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
-    ).to_dict()
+    def get_url_params(self, context: dict | None, next_page_token: t.Any | None) -> dict[str, t.Any]:
+        params: dict = {"limit": self.API_LIMIT_PAGE_SIZE}
+        if next_page_token:
+            params["offset"] = next_page_token
+        if self.replication_key:
+            params["order_by"] = f'+{self.replication_key}'
+        self.logger.info(f"QUERY PARAMS: {params}")
+        return params
